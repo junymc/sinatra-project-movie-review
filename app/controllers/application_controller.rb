bfb1220c4e63ebbd68727d5fa091685c4df66456
github username <github email address>
2019-08-05 19:56:19 -0700

    setting up migration and controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
+require "./config/environment"
+
 class ApplicationController < Sinatra::Base
 
     configure do
         set :views, 'app/views'
         set :public_folder, 'public'
+        enable :seessions
+        set :session_secret, "password_security"
     end
 
     get '/' do
