96807de5b500265d8c491881f0d5aded1e61f592
github username <github email address>
2019-08-06 16:50:54 -0700

    fixing home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,23 @@ class ApplicationController < Sinatra::Base
     configure do
         set :views, 'app/views'
         set :public_folder, 'public'
-        enable :seessions
+        enable :sessions
         set :session_secret, "password_security"
     end
+
+    helpers do
+		def logged_in?
+			!!session[:user_id]
+		end
+
+		def current_user
+			User.find_by(session[:user_id])
+		end
+	end
 
     get "/" do
         erb :index
   end
+  
 
 end
